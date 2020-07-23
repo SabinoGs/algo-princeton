5c72f914c4a3265431119c9436dc4941829ccf3c
C1299699 Gustavo Sabino Mangabeira <c1299699@interno.bb.com.br>
2020-07-23 00:41:25 -0300

    Questao 2 - Com alguma explicação

--- semana1/Questions/q2.py
+++ semana1/Questions/q2.py
@@ -1,6 +1,30 @@
 class UnionFindCanonical():
 
     def __init__(self, number_of_elements):
+        """Pra dar conta de resolver essa questão é necessário, de alguma maneira, manter a contagem dos elementos dos componentes.
+
+        Se tentarmos selecionar os elementos de um Connected Component (CC) e varrer eles, será uma busca, no mínimo, linear.
+
+        Agora, se atualizarmos a lista de contagem, toda vez que realizarmos uma union(a,b), 
+            vamos economizar um pouco de tempo mantendo contagem da lista. 
+
+        Mas como?
+
+        inicializamos uma lista nova de contagem, ex:
+
+        large = 0,1,2,3,4,5,6,7,8,9 <- Pois há 10 componentes conectados. 
+        Quando realizarmos um union(a,b) verificamos se large[a] > large[b], se sim large[root(b)] == large[a]. E vice-versa.
+
+        Mas porque adicionamos o root(a) no indice do large? 
+         R: Pq assim não temos que atualizar a lista large inteira. 
+            Atualiza somente em 1 lugar toda vez que realizamos a union. 
+            Assim o método find(a) deverá buscar qual é o número da lista `large` no índice root(a): large[root(a)]
+
+            (opinião): 
+                Acabamos com a lista large desatualizada nos indices não root. 
+                Mas o root sempre vai ter o elemento canônico que é esperado
+        """
+
         self.connected_components_id = list(range(number_of_elements))
         self.large = list(range(number_of_elements))
 
@@ -25,6 +49,9 @@ class UnionFindCanonical():
         return self.large[self.root(component)]
 
     def union(self, component_a, component_b):
+
+        print(" -> unindo ", component_a, component_b)
+
         root_a = self.root(component_a)
         root_b = self.root(component_b)
 
@@ -39,11 +66,14 @@ class UnionFindCanonical():
             self.connected_components_id[root_b] = self.connected_components_id[root_a]
             self.trees_sizes[root_a] += self.trees_sizes[root_b]
 
-        # Porque isso acontece? 
+        # Porque isso acontece?
         if large_a > large_b:
             self.large[root_b] = large_a
         else:
             self.large[root_a] = large_b
+
+        print(" -> Componentes: ", self.connected_components_id,
+              "Large: ", self.large)
 
 
 if __name__ == "__main__":
